445792a78e9a73b064922df295eb32874557bc92
Gilles <gilles@coolen.dev>
2021-04-09 17:55:22 +0200

    Remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AttemptModule } from './attempt/attempt.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtGuard } from './auth/guards/jwt-auth.guard';
 import { JwtStrategy } from './auth/strategies/jwt.strategy';
 import { RolesGuard } from './common/guards/role.guard';
 import { DatabaseModule } from './database/database.module';
+import { HintModule } from './hint/hint.module';
+import { TargetModule } from './target/target.module';
 import { UserModule } from './user/user.module';
 import { validate } from './util/env.validation';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validate }), DatabaseModule, UserModule, AuthModule],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ validate }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    AttemptModule,
+    HintModule,
+    TargetModule
+  ],
   providers: [
-    AppService,
     JwtStrategy,
     {
       provide: APP_GUARD,
